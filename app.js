5f175c16430f6b1e3e5e8bc08d50ea6825bea505
Carlos Vazquez <c.vazquez1986@gmail.com>
2023-04-11 19:54:54 -0400

    adding compose content to an array

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", (req, res) => {
   res.render("home", {
-    startingContent: homeStartingContent
+    startingContent: homeStartingContent,
+    posts: posts
   });
 });
 
@@ -38,7 +41,14 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 
